7d698ec039d852c3d711cb2cccc49d0f44f946f5
yucef sourani <yucefsourani@users.noreply.github.com>
2020-07-22 18:34:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,27 @@
 
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+
+setup(
+      name="pybigsurgnometweak",
+      description='pybigsurgnometweak',
+      long_description='pybigsurgnometweak.',
+      version="0.1",
+      author='Youcef Sourani',
+      author_email='youssef.m.sourani@gmail.com',
+      url="https://github.com/yucefsourani/pybigsurgnometweak",
+      license='GPLv3 License',
+      platforms='Linux',
+      scripts=['pybigsurgnometweak.py'],
+      keywords=['gnome', 'tweak'],
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: POSIX :: Linux',
+          'Development Status :: 4 - Beta ',
+          'Intended Audience :: End Users/Desktop',
+            ]
+)
